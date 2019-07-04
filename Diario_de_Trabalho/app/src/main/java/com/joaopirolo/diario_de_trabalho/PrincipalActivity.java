6b0be3c07da7c3bfe6360394e1bcb34ca9d4f32a
JoaoPirolo <jc_pirolo@hotmail.com>
2019-07-04 16:59:50 -0300

    barra de funcionalidades e infoActivity

--- Diario_de_Trabalho/app/src/main/java/com/joaopirolo/diario_de_trabalho/PrincipalActivity.java
+++ Diario_de_Trabalho/app/src/main/java/com/joaopirolo/diario_de_trabalho/PrincipalActivity.java
@@ -2,6 +2,8 @@ package com.joaopirolo.diario_de_trabalho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
     }
 }
